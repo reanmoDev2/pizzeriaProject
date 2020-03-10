c29d88e41469b3439186033e8fd4c7dc0ae73c6a
reanmoNiklas <61664856+reanmoDev2@users.noreply.github.com>
2020-03-10 13:12:49 +0100

    compiled bootstrap scss-folder added

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js" integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo SITENAME; ?></title>
 </head>
+
 <body>
-    
