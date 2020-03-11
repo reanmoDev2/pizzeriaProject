c1bf3165134ce6ff093bbfff09adab3f00249a17
reanmoNiklas <61664856+reanmoDev2@users.noreply.github.com>
2020-03-11 10:26:29 +0100

    Navbar with bell and user icon done - Dashboard next

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,21 @@
+<nav id="customNavbar" class="navbar">
+  <div id="navbarSymbols">
+    <ul class="nav navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link bell">
+          <svg xmlns="http://www.w3.org/2000/svg" width="27" height="23.999" viewBox="0 0 27 23.999">
+            <path id="bell-alt" d="M20.333,17.16c-1.04-1.04-2.339-2.341-2.339-7.409a7.505,7.505,0,0,0-6.22-7.393l-.045-.006a1.5,1.5,0,1,0-2.467,0l0-.005a7.513,7.513,0,0,0-6.266,7.4c0,5.068-1.3,6.367-2.339,7.409A2.252,2.252,0,0,0,2.245,21H7.494a3,3,0,1,0,6,0h5.248a2.252,2.252,0,0,0,1.591-3.84Z" transform="translate(0.001 -0.001)" fill="#353535" />
+            <circle id="Ellipse_19" data-name="Ellipse 19" cx="3" cy="3" r="3" transform="translate(21)" fill="#009046" />
+          </svg>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+            <circle id="Ellipse_2" data-name="Ellipse 2" cx="16" cy="16" r="16" fill="#353535" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
--- app/views/pages/dashboard.php
+++ app/views/pages/dashboard.php
@@ -1,4 +1,5 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php require APPROOT . '/views/inc/sidebar.php'; ?>
+<?php require APPROOT . '/views/inc/navbar.php'; ?>
 Dashboard
 <?php require APPROOT . '/views/inc/footer.php'; ?>
